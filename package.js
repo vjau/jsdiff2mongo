1ca82bda0314117aa67d6cf90a63a8599f337b5a
vjau <vjau@users.noreply.github.com>
2015-04-07 14:55:02 +0200

    Rename file names in package.js

--- package.js
+++ package.js
@@ -8,12 +8,12 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
   api.use('underscore');
-  api.addFiles('vjau:jsondiff2mongo.js');
+  api.addFiles('jsondiff2mongo.js');
   api.export('jsDiff2Mongo');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('vjau:jsondiff2mongo');
+  api.use('jsondiff2mongo');
   api.addFiles('vjau:jsondiff2mongo-tests.js');
 });
