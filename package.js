36c6e8d512a425f96fb846a27b0c8cb2fa3d9aca
Vincent Jaubert <contact@vjaubert.com>
2015-05-15 00:34:06 +0200

    cleanups && typos

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'vjau:jsdiff2mongo',
-  summary: ' Compares two objects an returns a diff with a mongo update query ',
-  version: '1.0.0',
+  summary: ' Compares two objects and returns a diff with a mongo update query ',
+  version: '1.0.2',
   git: ' https://github.com/vjau/jsdiff2mongo '
 });
 
