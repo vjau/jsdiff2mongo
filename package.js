5b95adb08adfef24895a5039f50bb6a721cea290
Vincent Jaubert <contact@vjaubert.com>
2015-09-05 16:45:21 +0200

    missing EJSON dependency in tests

--- package.js
+++ package.js
@@ -15,5 +15,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('vjau:jsdiff2mongo');
+  api.use('ejson');
   api.addFiles('jsdiff2mongo-tests.js');
 });
